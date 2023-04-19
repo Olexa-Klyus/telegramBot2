11af91a6245b3c0f0d61ba060f8d58b636435302
olexa <olexakljus@gmail.com>
2023-04-19 15:28:08 +0300

    test bot

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, request
 import telebot
-import os
+# import os
 
-app = Flask(__name__)
-TOKEN = os.environ.get('TOKEN')
-bot = telebot.TeleBot(TOKEN)
+# app = Flask(__name__)
+# TOKEN = os.environ.get('TOKEN')
+bot = telebot.TeleBot("6109114656:AAGOYhkO4H_22FJZabEJ7xh_KDYGUDLbbVQ")
 
 
 @bot.message_handler(commands=["start"])
@@ -31,18 +31,20 @@ def message_text(message):
     bot.send_message(message.chat.id, "Python")
 
 
-@app.route("/" + TOKEN, methods=["POST"])
-def get_message():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "Python Telegram Bot", 200
+bot.polling()
+
+# @app.route("/" + TOKEN, methods=["POST"])
+# def get_message():
+#     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+#     return "Python Telegram Bot", 200
 
 
-@app.route("/")
-def main():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://telebot2.herokuapp.com/" + TOKEN)
-    return "Python Telegram Bot", 200
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# @app.route("/")
+# def main():
+#     bot.remove_webhook()
+#     bot.set_webhook(url="https://telebot2.herokuapp.com/" + TOKEN)
+#     return "Python Telegram Bot", 200
+#
+#
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
